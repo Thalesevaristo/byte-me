2d7e9addeaaf1347bd3921d0138c820ed9580d18
Thales Evaristo <thalesevaristo@gmail.com>
2025-08-06 17:40:00 -0300

    CHORE: Rewrite the routes

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -1,20 +1,30 @@
 from flask import Blueprint, request
 from flask_jwt_extended import create_access_token
 from src.models import db, User
+from src.app import bcrypt
+
 from http import HTTPStatus
 
 app = Blueprint("auth", __name__, url_prefix="/auth")
 
 
+def check_password(password_hash, raw_password):
+    return bcrypt.check_password_hash(
+        pw_hash=password_hash,
+        password=raw_password,
+    )
+
+
 @app.route("/login", methods=["POST"])
 def login():
-    username = request.json.get("username", None)
-    password = request.json.get("password", None)
+    data = request.get_json()
+    username = data.get("username", None)
+    password = data.get("password", None)
 
     query = db.select(User).where(User.username == username)
     user = db.session.execute(query).scalar()
 
-    if not user or user.password != password:
+    if not user or not check_password(user.password, password):
         return {
             "msg": "Wrong name or password. Try again.",
         }, HTTPStatus.UNAUTHORIZED
